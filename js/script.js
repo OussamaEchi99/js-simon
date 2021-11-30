c9ef5fbd6ecfb1f2d1463366bf8a99b0f9c95905
OussamaEchi99 <oussama.echi99@gmail.com>
2021-11-30 16:26:38 +0100

    Finito

--- js/script.js
+++ js/script.js
@@ -2,20 +2,28 @@
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+// Variabile numeri
 const numbers = [ 2, 7, 6, 11, 17 ];
 
+// Con un ciclo For Scrivi all'utente ogni numero dell'array
 let numberArray;
 for (let i = 0; i < numbers.length; i++) {
     numberArray = numbers[i];
     alert(numberArray);
 }
 
+// Creo il timer della funzione
 setTimeout(userInput, 3000);
 
 
 let userNumber;
+// Creo la funzione che verrà chiamata al finire del timer
 function userInput() {
+    // Creo un'array dove pusho ogni numero dell'utente
     let userArray = [];
+    // Creo un'array dove pusho solo i numeri idovinati
+    let correctNumbers = [];
+    // Faccio un For per 5 volte per l'input dell'utente
     for (let i = 0; i < 5 ; i++) {
         userNumber = parseInt(prompt('Scivi il Primo numero'));
 
@@ -23,15 +31,21 @@ function userInput() {
     }
 
     let theFiveNumbers;
+    // Creo un For dove posso mettere a confronto i numeri del sistema
+    // e i numeri dati dall'utente
     for (let i = 0; i < userArray.length; i++) {
         theFiveNumbers = userArray[i];
-        if ( numberArray === theFiveNumbers ) {
-            alert('Hai indovinato il numero!')
+        if ( numbers.includes(theFiveNumbers) ) {
+            correctNumbers.push(theFiveNumbers);
         }
-    
     }
-    console.log(userArray);
-    console.log(theFiveNumbers);
+    
+    // Faccio l'output all'utente del risultato
+    const result = `
+    Hai indovinato ${correctNumbers.length} numeri.
+    E sono ${correctNumbers}                                
+    `
+    alert(result);
 }
 
 
